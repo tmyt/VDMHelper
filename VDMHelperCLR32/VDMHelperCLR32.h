7cfa663c0065621ec2a3d54a343d8c6caf1ff23c
yutaka <shigure@refy.net>
2015-09-07 20:22:18 +0900

    Initialize repository.

---
+++ VDMHelperCLR32/VDMHelperCLR32.h
@@ -0,0 +1,13 @@
+// VDMHelperCLR32.h
+
+#pragma once
+
+using namespace System;
+
+namespace VDMHelperCLR32 {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ VDMHelperCLR64/VDMHelperCLR64.h
@@ -0,0 +1,13 @@
+// VDMHelperCLR64.h
+
+#pragma once
+
+using namespace System;
+
+namespace VDMHelperCLR64 {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
