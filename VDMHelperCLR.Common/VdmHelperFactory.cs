66953e9152d56ee34382b0a243b1480f50634a05
yutaka <shigure@refy.net>
2015-09-11 22:40:35 +0900

    Add Factory class for IVdmHelper. #4

---
+++ VDMHelperCLR.Common/VdmHelperFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace VDMHelperCLR.Common
+{
+    public static class VdmHelperFactory
+    {
+        private static string DllName32 = "VDMHelperCLR32.dll";
+        private static string DllName64 = "VDMHelperCLR32.dll";
+
+        private static string GetPlatformDllName()
+        {
+            var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\";
+            return dir + (!Environment.Is64BitProcess ? DllName32 : DllName64);
+        }
+
+        public static IVdmHelper CreateInstance()
+        {
+            var asm = Assembly.LoadFile(GetPlatformDllName());
+            var type = asm.GetType("VDMHelperCLR.VdmHelper");
+            return (IVdmHelper)Activator.CreateInstance(type);
+        }
+    }
+}
